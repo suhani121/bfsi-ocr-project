4ed8da0f300c7853dd8ba46978a441008ed22fc5
suhani121 <153366322+suhani121@users.noreply.github.com>
2025-01-26 13:30:19 +0530

    Add files via upload

---
+++ bfsi-ocr-project/supervised/classification_analysis.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+output_file = "unsupervised\classified_data.csv"
+df = pd.read_csv(output_file)
+
+category_counts = df['predicted_category'].value_counts()
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(8, 8))
+plt.pie(
+    category_counts, 
+    labels=category_counts.index, 
+    autopct='%1.1f%%', 
+    startangle=140, 
+    colors=plt.cm.tab20.colors
+)
+plt.title('Distribution of Predicted Categories')
+plt.axis('equal')  
+plt.show()
